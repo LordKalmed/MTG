20b62ea87a1600aca062b1f95b59707bee992bf5
LordKalmed <angust.cloud@gmail.com>
2021-11-10 00:24:01 +0000

    large changes to files. set the overall structure, python script, functional mysql commands. Basic functions all there.

--- apps/route.py
+++ apps/route.py
@@ -1,83 +1,82 @@
 from flask import Flask, redirect, render_template, request
-from flask_sqlalchemy import SQLAlchemy
-from os import getenv
-from flask_wtf import FlaskForm
-from wtforms import IntegerField, StringField, SelectField, SubmitField
+import mysql.connector
 
+db=mysql.connector.connect (    host="localhost" ,  
+                                user="root",
+                                password="root",
+                                database="tournament")  #exposed SQL details security issue.
 
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = getenv('secretkey')
-
-db = SQLAlchemy(app)
-
-class players(db.Model):
-    pid = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(50))
-    surname = db.Column(db.String(50))
-    age = db.Column(db.Integer)
-    email = db.Column(db.String(50))
-
-class score(db.Model):
-    pid = db.Column(db.Integer, primary_key = True)
-    score1 = db.Column(db.String(50))
-    score2 = db.Column(db.Integer)
-    score3 = db.Column(db.Integer)
-    score4 = db.Column(db.String(50))
-
-db.drop_all()
-db.create_all()
-                                                                                     #get db connection to mysql with SQL alchemy
+app=Flask(__name__, template_folder='../templates')
+#get db connection to mysql with SQL alchemy if possible for security.
 cursor=db.cursor()
 
 #class forms instead of html!
-class AddEmp(FlaskForm):
-    emp_name = StringField("Name")
-    salary = IntegerField("Salary")
-    marks = IntegerField("Marks")
-    subject = SelectField("Subject", choices=[('python', 'Python'), ('java', 'Java'), ('php', 'PHP'), ('sql', 'SQL')])
-    department = SelectField('Department', choices=[('IT', 'Information technology'), ('HR', 'Human Resources'), ('sales', 'Sales'), ('training', 'Training')])
-    submit = SubmitField('Add Employee')
+#class AddEmp(FlaskForm):
+    #salary = IntegerField("Salary")
+    #marks = IntegerField("Marks")
+    #subject = SelectField("Subject", choices=[('python', 'Python'), ('java', 'Java'), ('php', 'PHP'), ('sql', 'SQL')])
+    #department = SelectField('Department', choices=[('IT', 'Information technology'), ('HR', 'Human Resources'), ('sales', 'Sales'), ('training', 'Training')])
+    #submit = SubmitField('Add Employee')
 
-@app.route("/")                                                                             #get high score data for leader board
+@app.route("/")                                                             #Mostly just hyper links and info
 def homepage():
-    return render_template("/home.html")
+    return render_template('home.html')
 
-@app.rout("/submitplayer", methods=["POST"])                                                #should now recieve post with all players details
+@app.route("/submitplayer", methods=['GET', 'POST'])                        #functional code that inserts the user values as a new player
 def newplayer():
     cursor.execute("select ifnull(max(pid),100)+1 from player")
-    newstudentid=cursor.fetchone()
+    newpid=cursor.fetchone()
     name=request.form["name"]
     surname=request.form["surname"]
     age=request.form["age"]
     email=request.form["email"]
-    sqlquery="insert into players value({0},'{1}','{2}',{3}, '{4}')".format(newstudentid[0],name,surname,age, email)
+    sqlquery="insert into player value({0},'{1}','{2}','{3}', {4})".format(newpid[0],name,surname,email, age)
     cursor.execute(sqlquery)
     db.commit()
     return redirect("/")
 
-@app.route("/highscore")                                                                    #got the correct sql query for score calc.
+@app.route("/highscore", methods=['GET', 'POST'])                           #got the correct sql query for score calc. Desc order for easy winner
 def highscore():
-    cursor.execute("select pid, sum(score1+score2+score3+score4) from  score group by pid order by pid desc")                                                 #get sql quesry to display highest decesding
-    #####(select t.pid,u.name, sum(score1+score2+score3+score4) from score t inner join player u on t.pid = u.pid  group by pid order by pid desc)                                                                                   #need to figure out how to swap player_id for name here
+    cursor.execute("select t.pid,u.name, sum(score1+score2+score3+score4) from score t inner join player u on t.pid = u.pid  group by pid order by pid desc")                                                 #get sql quesry to display highest decesding
     data=cursor.fetchall()
-    render_template("highscore.html", records=data)                                         #sending all the sql query results to page
+    return render_template('highscore.html',records=data)                                         #sending all the sql query results to page
 
-@app.route("/addplayer", methods=["POST"])                                                  #function is sent a player id to add to the score table.
+@app.route("/addplayer", methods=['GET', 'POST'])                           #makes a table of all players to add to tournament(score)
 def addplayer():
     cursor.execute("select pid,name,surname from player")
     data=cursor.fetchall()
-    render_template("addplayer.html", records=data)
+    return render_template('addplayer.html', records=data)
 
 
-@app.route("/submitaddplayer/<pid>")
+@app.route("/submitaddplayer/<pid>")                                        #function to add a player to the score table with 0 score.
 def subaddplayer(pid):
-    cursor.execute("insert into scores value ('{0}', {1}, {2}, {3})".format(pid, 0,0,0))      #may need if/else for double entry
+    cursor.execute("insert into score value ({0}, {1}, {2}, {3}, {4})".format(pid, 0,0,0,0))      #may need if/else for double entry
     db.commit()
-    return redirect("addplayer.html")
+    return redirect("/addplayer")
 
+@app.route("/newplayerdetails")                                             #functional page that send player details to /submitplayer
+def newplayerdetails():
+    return render_template('newplayer.html')  
+
+@app.route('/editscoresub/<pid>', methods=["POST", "GET"])
+def editscoresub(pid):
+    pid=request.form["pid"]
+    score1=request.form["score1"]
+    score2=request.form["score2"]
+    score3=request.form["score3"]
+    score4=request.form["score4"]
+    sqlquery="update score set score1 = {1}, score2 = {2}, score3 = {3}, score4 = {4} where pid={0}".format(pid, score1, score2, score3, score4)
+    cursor.execute(sqlquery)
+    db.commit
+    return redirect('/highscore')
+
+@app.route('/updatescore/<pid>')
+def editscore(pid):
+    cursor.execute("select * from score where pid={0}".format(pid))
+    data=cursor.fetchall()
+    cursor.execute("select name from player where pid={0}".format(pid))
+    name=cursor.fetchall()
+    return render_template('updatescore.html', record=data, player=name)
 
 
 
