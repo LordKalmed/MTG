ff77c4a3f31a2c94ef2798724f30419572e71a4e
LordKalmed <angust.cloud@gmail.com>
2021-11-15 08:23:50 +0000

    project not ready but out of time

--- apps/test.py
+++ apps/test.py
@@ -1,25 +1,38 @@
+import flask
 from mysql import connector
 from flask import Flask, redirect, render_template, request
+from werkzeug.wrappers.response import Response
 import route
 import mysql.connector
 import pytest
+import requests
 
 
 db=mysql.connector.connect (    host="localhost" ,  
                                 user="root", 
-                                password="rootroot", 
+                                password="root", 
                                 database="tournament_test")
 
 cursor=db.cursor() 
 
-cursor.execute( "create table player_test (pid int, name char(25), surname char(25), email char(50), age int)")
+drop1="drop table if exists player"
+drop2="drop table if exists score"
+cursor.execute(drop1)
+cursor.execute(drop2)
+db.commit
+
+
+
+cursor.execute( "create table player (pid int, name char(25), surname char(25), email char(50), age int)")
 db.commit
 cursor.execute( "create table score (pid int, score1 int, score2 int, score3 int, score4 int)")
 db.commit
 
-def test_homepage(self):
-    self.app.get('/')
-    self.assert_template_used('home.html')
+def test_homepage():
+    "GET request to url returns a 200"
+    url = 'https://monitorial.com/'
+    resp = requests.get(url)
+    assert resp.status_code == 200
 
 def test_newplayer(self):
         response = self.client.post("/submitplayer"),
